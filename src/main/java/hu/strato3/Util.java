c13836e303941ec53acd1f82cdf141007e71cab6
Dave <zibriczky.david@gravityrd.com>
2013-10-24 17:24:29 +0200

    Utils updated

--- src/main/java/hu/strato3/Util.java
+++ src/main/java/hu/strato3/Util.java
@@ -4,6 +4,8 @@
  */
 package hu.strato3;
 
+import Jama.Matrix;
+
 /**
  *
  * @author DaveXster
@@ -32,5 +34,30 @@ public class Util {
 	
 	public static void addRegularization(double[][] M, double r) {
 		for (int i = 0; i < M.length; i++) { M[i][i] += r; }
+	}
+	
+	public static String getMatrixString(double[][] M) {
+		String res = "";
+		for (int i = 0; i < M.length; i++) {
+			for (int j = 0; j < M.length; j++) {
+				res += String.format("[%.6f]", M[i][j]);
+			}
+			res += "\n";
+		}
+		return res;
+	}
+	public static String getVectorString(double[] v) {
+		String res = "";
+		for (int i = 0; i < v.length; i++) {
+			res += String.format("[%.6f]", v[i]);
+		}
+		return res;
+	}
+	public static double[][] toDoubleArrayArray(Matrix m) {
+		double[][] res = new double[m.getRowDimension()][m.getColumnDimension()];
+		for (int i = 0; i < res.length; i++) {
+			res[i] = m.getRowPackedCopy();
+		}
+		return res;
 	}
 }
