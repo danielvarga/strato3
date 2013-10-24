8633aaaf24b8e0f7c4f78c0d4d6a345b74f894e6
Dave <zibriczky.david@gravityrd.com>
2013-10-24 17:06:56 +0200

    Added Util class

---
+++ src/main/java/hu/strato3/Util.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.strato3;
+
+/**
+ *
+ * @author DaveXster
+ */
+public class Util {
+
+	public static void incrementMatrix(double[][] M, double[] v) {
+		for (int i = 0; i < v.length; i++) {
+			double[] mi = M[i];
+			double vi = v[i];
+			for (int j = i; j < v.length; j++) { mi[j] += vi * v[j]; }
+		}
+	}
+	
+	public static void incrementVector(double[] out, double[] v, double r) {
+		for (int i = 0; i < v.length; i++) { out[i] += v[i] * r; }
+	}
+	
+	public static void fillLowerMatrix(double[][] M) {
+		for (int i = 0; i < M.length; i++) {
+			for (int j = i; j < M.length; j++) {
+				M[j][i] = M[i][j];
+			}
+		}
+	}
+	
+	public static void addRegularization(double[][] M, double r) {
+		for (int i = 0; i < M.length; i++) { M[i][i] += r; }
+	}
+}
