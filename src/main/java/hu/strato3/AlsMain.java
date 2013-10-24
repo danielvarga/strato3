560208dcebcce2409298c8427272f6a366dbdc77
Dave <dave@gravityrd.com>
2013-10-24 15:24:54 +0200

    Added TokenizeLineFactors

--- src/main/java/hu/strato3/AlsMain.java
+++ src/main/java/hu/strato3/AlsMain.java
@@ -15,6 +15,7 @@ import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
 import eu.stratosphere.pact.common.stubs.Collector;
 import eu.stratosphere.pact.common.stubs.MapStub;
 import eu.stratosphere.pact.common.type.PactRecord;
+import eu.stratosphere.pact.common.type.base.PactBoolean;
 import eu.stratosphere.pact.common.type.base.PactInteger;
 import eu.stratosphere.pact.common.type.base.PactDouble;
 import eu.stratosphere.pact.common.type.base.PactString;
@@ -31,8 +32,6 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 		private final PactInteger second = new PactInteger(0);
 		private final PactDouble value = new PactDouble(0.0);
 
-		private final AsciiUtils.WhitespaceTokenizer tokenizer = new AsciiUtils.WhitespaceTokenizer();
-
 		@Override
 		public void map(PactRecord record, Collector<PactRecord> collector) {
 			// get the first field (as type PactString) from the record
@@ -48,18 +47,45 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 		}
 	}
 
+	public static class TokenizeLineFactors extends MapStub implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		// initialize reusable mutable objects
+		private final PactRecord outputRecord = new PactRecord();
+
+		@Override
+		public void map(PactRecord record, Collector<PactRecord> collector) {
+			// get the first field (as type PactString) from the record
+			PactString line = record.getField(0, PactString.class);
+			String[] splitted = line.getValue().split("\\|");
+			
+			int id = Integer.parseInt(splitted[0]);
+			this.outputRecord.setField(0, new PactInteger(id));
+			for (int i = 1; i < splitted.length; i++) {
+				double value = Double.parseDouble(splitted[i]);
+				this.outputRecord.setField(i, new PactDouble(value));
+			}
+			collector.collect(this.outputRecord);
+		}
+	}	
+	
 	@Override
 	public Plan getPlan(String... args) {
 		// parse job parameters
 		int numSubTasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
 		String dataInput = (args.length > 1 ? args[1] : "");
-		String output = (args.length > 2 ? args[2] : "");
+		String dataInputFactors = (args.length > 2 ? args[2] : "");
+		String output = (args.length > 3 ? args[3] : "");
 
-		FileDataSource source = new FileDataSource(new TextInputFormat(),
-				dataInput, "Input Lines");
-		source.setParameter(TextInputFormat.CHARSET_NAME, "ASCII"); 
-		MapContract mapper = MapContract.builder(new TokenizeLine())
-				.input(source).name("Tokenize Lines").build();
+		FileDataSource source = new FileDataSource(new TextInputFormat(), dataInput, "Input Lines");
+		FileDataSource sourceFactors = new FileDataSource(new TextInputFormat(), dataInputFactors, "Input Factor Lines");
+		source.setParameter(TextInputFormat.CHARSET_NAME, "ASCII");
+		sourceFactors.setParameter(TextInputFormat.CHARSET_NAME, "ASCII"); 
+		MapContract mapper = MapContract.builder(new TokenizeLine()) .input(source).name("Tokenize Lines").build();
+		MapContract mapperFactors = MapContract.builder(new TokenizeLineFactors()).input(sourceFactors).name("Tokenize Lines").build();
+		
+		
+		
 		/*
 		 * ReduceContract reducer = ReduceContract.builder(CountWords.class,
 		 * PactString.class, 0) .input(mapper) .name("Count Words") .build();
@@ -85,12 +111,11 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 
 	public static void main(String[] args) throws Exception {
 		AlsMain als = new AlsMain();
-		Plan plan = als.getPlan(args[0], args[1], args[2]);
+		Plan plan = als.getPlan(args[0], args[1], args[2], args[3]);
 		// This will create an executor to run the plan on a cluster. We assume
 		// that the JobManager is running on the local machine on the default
 		// port. Change this according to your configuration.
-		PlanExecutor ex = new RemoteExecutor("localhost", 6123,
-				"target/pact-examples-0.4-SNAPSHOT-WordCount.jar");
+		PlanExecutor ex = new RemoteExecutor("localhost", 6123, "target/pact-examples-0.4-SNAPSHOT-WordCount.jar");
 		ex.executePlan(plan);
 	}
 }
