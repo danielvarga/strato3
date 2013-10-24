7d40c2a67bec59772746e95b59340afbb3a6441e
Zsolt Fekete <zsfekete@info.ilab.sztaki.hu>
2013-10-24 17:20:32 +0200

    update buid.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 strato_dir="../stratosphere"
 
-javac -cp "$strato_dir"/'stratosphere-dist/target/stratosphere-dist-0.4-SNAPSHOT-bin/stratosphere-0.4-SNAPSHOT/lib/*' src/main/java/hu/strato3/*.java 
+javac -cp "$strato_dir"/'stratosphere-dist/target/stratosphere-dist-0.4-SNAPSHOT-bin/stratosphere-0.4-SNAPSHOT/lib/*':lib/* src/main/java/hu/strato3/*.java 
 
 jar cvf wordcount.jar -C src/main/java .
 
