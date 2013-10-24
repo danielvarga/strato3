470265f8b4c8fe9f65e2661fa4a1642218c5555d
Zsolt Fekete <zsfekete@info.ilab.sztaki.hu>
2013-10-24 14:38:30 +0200

    Als wip

--- run_wordcount.sh
+++ run_wordcount.sh
@@ -1,3 +1,5 @@
 
 
-../stratosphere/stratosphere-dist/target/stratosphere-dist-0.4-SNAPSHOT-bin/stratosphere-0.4-SNAPSHOT/bin/pact-client.sh run -j wordcount.jar -c eu.stratosphere.pact.example.wordcount.WordCount -a 1 file:///home/zsfekete/hamlet.txt file:///home/zsfekete/wordcount_output1
+#../stratosphere/stratosphere-dist/target/stratosphere-dist-0.4-SNAPSHOT-bin/stratosphere-0.4-SNAPSHOT/bin/pact-client.sh run -j wordcount.jar -c eu.stratosphere.pact.example.wordcount.WordCount -a 1 file:///home/zsfekete/hamlet.txt file:///home/zsfekete/wordcount_output1
+
+../stratosphere/stratosphere-dist/target/stratosphere-dist-0.4-SNAPSHOT-bin/stratosphere-0.4-SNAPSHOT/bin/pact-client.sh run -j wordcount.jar -c hu.strato3.AlsMain -a 1 file:///home/zsfekete/hamlet.txt file:///home/zsfekete/wordcount_output1
