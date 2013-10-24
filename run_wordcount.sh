c541e214a9abee281ff69a33275296da2bdc28d4
Zsolt Fekete <zsfekete@info.ilab.sztaki.hu>
2013-10-24 15:15:10 +0200

    read R runs

--- run_wordcount.sh
+++ run_wordcount.sh
@@ -2,4 +2,4 @@
 
 #../stratosphere/stratosphere-dist/target/stratosphere-dist-0.4-SNAPSHOT-bin/stratosphere-0.4-SNAPSHOT/bin/pact-client.sh run -j wordcount.jar -c eu.stratosphere.pact.example.wordcount.WordCount -a 1 file:///home/zsfekete/hamlet.txt file:///home/zsfekete/wordcount_output1
 
-../stratosphere/stratosphere-dist/target/stratosphere-dist-0.4-SNAPSHOT-bin/stratosphere-0.4-SNAPSHOT/bin/pact-client.sh run -j wordcount.jar -c hu.strato3.AlsMain -a 1 file:///home/zsfekete/hamlet.txt file:///home/zsfekete/wordcount_output1
+../stratosphere/stratosphere-dist/target/stratosphere-dist-0.4-SNAPSHOT-bin/stratosphere-0.4-SNAPSHOT/bin/pact-client.sh run -j wordcount.jar -c hu.strato3.AlsMain -a 1 file:///home/$USER/git/strato3/data/test_small.csv file:///home/zsfekete/wordcount_output1
--- src/main/java/hu/strato3/AlsMain.java
+++ src/main/java/hu/strato3/AlsMain.java
@@ -20,30 +20,27 @@ import eu.stratosphere.pact.common.type.base.PactDouble;
 import eu.stratosphere.pact.common.type.base.PactString;
 import eu.stratosphere.pact.example.util.AsciiUtils;
 
+public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 
-public class AlsMain  implements PlanAssembler, PlanAssemblerDescription {
-	
-
-  public static class TokenizeLine extends MapStub implements Serializable {
+	public static class TokenizeLine extends MapStub implements Serializable {
 		private static final long serialVersionUID = 1L;
-		
+
 		// initialize reusable mutable objects
 		private final PactRecord outputRecord = new PactRecord();
 		private final PactInteger first = new PactInteger(0);
 		private final PactInteger second = new PactInteger(0);
 		private final PactDouble value = new PactDouble(0.0);
-		
-		private final AsciiUtils.WhitespaceTokenizer tokenizer =
-				new AsciiUtils.WhitespaceTokenizer();
-		
+
+		private final AsciiUtils.WhitespaceTokenizer tokenizer = new AsciiUtils.WhitespaceTokenizer();
+
 		@Override
 		public void map(PactRecord record, Collector<PactRecord> collector) {
 			// get the first field (as type PactString) from the record
 			PactString line = record.getField(0, PactString.class);
 			String[] splitted = line.getValue().split("\\|");
-                        first.setValue(Integer.parseInt(splitted[0]));
-                        second.setValue(Integer.parseInt(splitted[1]));
-                        value.setValue(Double.parseDouble(splitted[2]));
+			first.setValue(Integer.parseInt(splitted[0]));
+			second.setValue(Integer.parseInt(splitted[1]));
+			value.setValue(Double.parseDouble(splitted[2]));
 			this.outputRecord.setField(0, this.first);
 			this.outputRecord.setField(1, this.second);
 			this.outputRecord.setField(2, this.value);
@@ -51,33 +48,28 @@ public class AlsMain  implements PlanAssembler, PlanAssemblerDescription {
 		}
 	}
 
-
-@Override
+	@Override
 	public Plan getPlan(String... args) {
 		// parse job parameters
-		int numSubTasks   = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
+		int numSubTasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
 		String dataInput = (args.length > 1 ? args[1] : "");
-		String output    = (args.length > 2 ? args[2] : "");
+		String output = (args.length > 2 ? args[2] : "");
 
-		FileDataSource source = new FileDataSource(new TextInputFormat(), dataInput, "Input Lines");
-		source.setParameter(TextInputFormat.CHARSET_NAME, "ASCII");		// comment out this line for UTF-8 inputs
+		FileDataSource source = new FileDataSource(new TextInputFormat(),
+				dataInput, "Input Lines");
+		source.setParameter(TextInputFormat.CHARSET_NAME, "ASCII"); 
 		MapContract mapper = MapContract.builder(new TokenizeLine())
-			.input(source)
-			.name("Tokenize Lines")
-			.build();
-                /*
-		ReduceContract reducer = ReduceContract.builder(CountWords.class, PactString.class, 0)
-			.input(mapper)
-			.name("Count Words")
-			.build();
-                        */
-		FileDataSink out = new FileDataSink(new RecordOutputFormat(), output, mapper, "sink");
-		RecordOutputFormat.configureRecordFormat(out)
-			.recordDelimiter('\n')
-			.fieldDelimiter(' ')
-			.field(PactString.class, 0)
-			.field(PactInteger.class, 1);
-		
+				.input(source).name("Tokenize Lines").build();
+		/*
+		 * ReduceContract reducer = ReduceContract.builder(CountWords.class,
+		 * PactString.class, 0) .input(mapper) .name("Count Words") .build();
+		 */
+		FileDataSink out = new FileDataSink(new RecordOutputFormat(), output,
+				mapper, "sink");
+		RecordOutputFormat.configureRecordFormat(out).recordDelimiter('\n')
+				.fieldDelimiter(',').field(PactInteger.class, 0)
+				.field(PactInteger.class, 1).field(PactDouble.class, 2);
+
 		Plan plan = new Plan(out, "ALS Example");
 		plan.setDefaultParallelism(numSubTasks);
 		return plan;
@@ -90,14 +82,15 @@ public class AlsMain  implements PlanAssembler, PlanAssemblerDescription {
 	public String getDescription() {
 		return "Parameters: [numSubStasks] [input] [output]";
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		AlsMain als = new AlsMain();
 		Plan plan = als.getPlan(args[0], args[1], args[2]);
 		// This will create an executor to run the plan on a cluster. We assume
 		// that the JobManager is running on the local machine on the default
 		// port. Change this according to your configuration.
-		PlanExecutor ex = new RemoteExecutor("localhost", 6123, "target/pact-examples-0.4-SNAPSHOT-WordCount.jar");
+		PlanExecutor ex = new RemoteExecutor("localhost", 6123,
+				"target/pact-examples-0.4-SNAPSHOT-WordCount.jar");
 		ex.executePlan(plan);
 	}
 }
