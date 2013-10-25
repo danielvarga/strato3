96b9e4dbe69d664499f068d06be6af319ed2fffd
Zsolt Fekete <zsfekete@info.ilab.sztaki.hu>
2013-10-25 11:02:47 +0200

    refactor

--- src/main/java/hu/strato3/AlsMain.java
+++ src/main/java/hu/strato3/AlsMain.java
@@ -46,7 +46,6 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 	public static class TokenizeLine extends MapStub implements Serializable {
 		private static final long serialVersionUID = 1L;
 
-		// initialize reusable mutable objects
 		private final PactRecord outputRecord = new PactRecord();
 		private final PactInteger first = new PactInteger(0);
 		private final PactInteger second = new PactInteger(0);
@@ -54,7 +53,6 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 
 		@Override
 		public void map(PactRecord record, Collector<PactRecord> collector) {
-			// get the first field (as type PactString) from the record
 			PactString line = record.getField(0, PactString.class);
 			String[] splitted = line.getValue().split("\\|");
 			first.setValue(Integer.parseInt(splitted[0]));
@@ -65,110 +63,6 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 			this.outputRecord.setField(2, this.value);
 			collector.collect(this.outputRecord);
 		}
-	}
-
-	@Override
-	public Plan getPlan(String... args) {
-		// parse job parameters
-		int numSubTasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
-		String dataInput = (args.length > 1 ? args[1] : "");
-		String output = (args.length > 2 ? args[2] : "");
-		int nFactors = Integer.parseInt((args.length > 3 ? args[3] : ""
-				+ nFactorsDef));
-		boolean printLogs = Boolean.parseBoolean((args.length > 4 ? args[4]
-				: "false"));
-		int nIterations = Integer.parseInt((args.length > 5 ? args[5] : ""
-				+ nIterationsDef));
-		double lambda = Double.parseDouble((args.length > 6 ? args[6] : ""
-				+ lambdaDef));
-		int targetIdx = 1;
-
-		FileDataSource source = new FileDataSource(new TextInputFormat(),
-				dataInput, "Input Lines");
-		source.setParameter(TextInputFormat.CHARSET_NAME, "ASCII");
-
-		MapContract ratingsInput = MapContract.builder(new TokenizeLine())
-				.input(source).name("Tokenize Lines").build();
-		ReduceContract factorsInput = ReduceContract
-				.builder(Init.class, PactInteger.class, 0).input(ratingsInput)
-				.name("Count Words").build();
-		factorsInput.setParameter(N_FACTORS, nFactors);
-
-		MatchContract match = MatchContract
-				.builder(UserItemRatingFactorMatch.class, PactInteger.class, 0,
-						0).input1(ratingsInput).input2(factorsInput)
-				.name("User-item-rating factors match").build();
-		match.setParameter(N_FACTORS, nFactors);
-
-		ReduceContract computeP = ReduceContract
-				.builder(Compute.class, PactInteger.class, targetIdx)
-				.input(match).name("LS solve").build();
-		computeP.setParameter(N_FACTORS, nFactors);
-		computeP.setParameter(LAMBDA, "" + lambda);
-		computeP.setParameter(TARGET_IDX, targetIdx);
-		computeP.setParameter(PRINT_LOGS, printLogs);
-
-		MatchContract match2 = MatchContract
-				.builder(UserItemRatingFactorMatch.class, PactInteger.class, 0,
-						0).input1(ratingsInput).input2(computeP)
-				.name("User-item-rating factors match").build();
-		match2.setParameter(N_FACTORS, nFactors);
-
-		ReduceContract computeQ = ReduceContract
-				.builder(Compute.class, PactInteger.class, 1 - targetIdx)
-				.input(match2).name("LS solve").build();
-		computeQ.setParameter(N_FACTORS, nFactors);
-		computeQ.setParameter(LAMBDA, "" + lambda);
-		computeQ.setParameter(TARGET_IDX, 1 - targetIdx);
-		computeQ.setParameter(PRINT_LOGS, printLogs);
-
-		// FileDataSink out = new FileDataSink(new RecordOutputFormat(), output,
-		// ratingsInput, "sink");
-		// FileDataSink out2 = new FileDataSink(new RecordOutputFormat(), output
-		// + "_q", factorsInput, "sink2");
-		// FileDataSink out3 = new FileDataSink(new RecordOutputFormat(), output
-		// + "_match", match, "sink3");
-		FileDataSink outP = new FileDataSink(new RecordOutputFormat(), output
-				+ "_solve_P", computeP, "P");
-		FileDataSink outQ = new FileDataSink(new RecordOutputFormat(), output
-				+ "_solve_Q", computeQ, "Q");
-
-		// RecordOutputFormat.configureRecordFormat(out).recordDelimiter('\n').fieldDelimiter(',')
-		// .field(PactInteger.class, 0).field(PactInteger.class,
-		// 1).field(PactDouble.class, 2);
-		//
-		// ConfigBuilder config2 =
-		// RecordOutputFormat.configureRecordFormat(out2).recordDelimiter('\n').fieldDelimiter(',')
-		// .field(PactInteger.class, 0);
-		// for (int i = 0; i < nFactors; i++) { config2 =
-		// config2.field(PactDouble.class, i+1); }
-		//
-		// ConfigBuilder config3 =
-		// RecordOutputFormat.configureRecordFormat(out3).recordDelimiter('\n').fieldDelimiter(',')
-		// .field(PactInteger.class, 0).field(PactInteger.class,
-		// 1).field(PactDouble.class, 2);
-		// for (int i = 0; i < nFactors; i++) { config3 =
-		// config3.field(PactDouble.class, i + 3); }
-
-		ConfigBuilder configP = RecordOutputFormat.configureRecordFormat(outP)
-				.recordDelimiter('\n').fieldDelimiter(',')
-				.field(PactInteger.class, 0);
-		for (int i = 0; i < nFactors; i++) {
-			configP = configP.field(PactDouble.class, i + 1);
-		}
-
-		ConfigBuilder configQ = RecordOutputFormat.configureRecordFormat(outQ)
-				.recordDelimiter('\n').fieldDelimiter(',')
-				.field(PactInteger.class, 0);
-		for (int i = 0; i < nFactors; i++) {
-			configQ = configQ.field(PactDouble.class, i + 1);
-		}
-
-		Plan plan = new Plan(outQ, "ALS Example");
-		plan.setDefaultParallelism(numSubTasks);
-		// return plan;
-		return new Plan(new ArrayList<GenericDataSink>(
-				Arrays.asList(new GenericDataSink[] { outP, outQ })));
 	}
 
 	public static class Init extends ReduceStub implements Serializable {
@@ -194,6 +88,32 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 				outputRecord.setField(j + 1, new PactDouble(val));
 			}
 			out.collect(outputRecord);
+		}
+	}
+
+	public static class UserItemRatingFactorMatch extends MatchStub implements
+			Serializable {
+
+		private static final long serialVersionUID = -5493679926356452044L;
+
+		private int nFactors;
+
+		@Override
+		public void open(Configuration conf) {
+			nFactors = conf.getInteger(N_FACTORS, nFactorsDef);
+		}
+
+		@Override
+		public void match(PactRecord ratings, PactRecord factors,
+				Collector<PactRecord> out) throws Exception {
+			PactRecord res = new PactRecord(nFactors + 3);
+			res.setField(0, ratings.getField(0, PactInteger.class));
+			res.setField(1, ratings.getField(1, PactInteger.class));
+			res.setField(2, ratings.getField(2, PactDouble.class));
+			for (int i = 0; i < nFactors; i++) {
+				res.setField(i + 3, factors.getField(i + 1, PactDouble.class));
+			}
+			out.collect(res);
 		}
 	}
 
@@ -273,28 +193,86 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 		}
 	}
 
-	public static class UserItemRatingFactorMatch extends MatchStub implements
-			Serializable {
+	@Override
+	public Plan getPlan(String... args) {
+		// parse job parameters
+		int numSubTasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
+		String dataInput = (args.length > 1 ? args[1] : "");
+		String output = (args.length > 2 ? args[2] : "");
+		int nFactors = Integer.parseInt((args.length > 3 ? args[3] : ""
+				+ nFactorsDef));
+		boolean printLogs = Boolean.parseBoolean((args.length > 4 ? args[4]
+				: "false"));
+		int nIterations = Integer.parseInt((args.length > 5 ? args[5] : ""
+				+ nIterationsDef));
+		double lambda = Double.parseDouble((args.length > 6 ? args[6] : ""
+				+ lambdaDef));
+		int targetIdx = 1;
 
-		int nFactors;
+		FileDataSource source = new FileDataSource(new TextInputFormat(),
+				dataInput, "Input Lines");
+		source.setParameter(TextInputFormat.CHARSET_NAME, "ASCII");
 
-		@Override
-		public void open(Configuration conf) {
-			nFactors = conf.getInteger(N_FACTORS, nFactorsDef);
+		MapContract ratingsInput = MapContract.builder(new TokenizeLine())
+				.input(source).name("Tokenize Lines").build();
+
+		ReduceContract factorsInput = ReduceContract
+				.builder(Init.class, PactInteger.class, 0).input(ratingsInput)
+				.name("Count Words").build();
+		factorsInput.setParameter(N_FACTORS, nFactors);
+
+		MatchContract match = MatchContract
+				.builder(UserItemRatingFactorMatch.class, PactInteger.class, 0,
+						0).input1(ratingsInput).input2(factorsInput)
+				.name("User-item-rating factors match").build();
+		match.setParameter(N_FACTORS, nFactors);
+
+		ReduceContract computeP = ReduceContract
+				.builder(Compute.class, PactInteger.class, targetIdx)
+				.input(match).name("LS solve").build();
+		computeP.setParameter(N_FACTORS, nFactors);
+		computeP.setParameter(LAMBDA, "" + lambda);
+		computeP.setParameter(TARGET_IDX, targetIdx);
+		computeP.setParameter(PRINT_LOGS, printLogs);
+
+		MatchContract match2 = MatchContract
+				.builder(UserItemRatingFactorMatch.class, PactInteger.class, 0,
+						0).input1(ratingsInput).input2(computeP)
+				.name("User-item-rating factors match").build();
+		match2.setParameter(N_FACTORS, nFactors);
+
+		ReduceContract computeQ = ReduceContract
+				.builder(Compute.class, PactInteger.class, 1 - targetIdx)
+				.input(match2).name("LS solve").build();
+		computeQ.setParameter(N_FACTORS, nFactors);
+		computeQ.setParameter(LAMBDA, "" + lambda);
+		computeQ.setParameter(TARGET_IDX, 1 - targetIdx);
+		computeQ.setParameter(PRINT_LOGS, printLogs);
+
+		FileDataSink outP = new FileDataSink(new RecordOutputFormat(), output
+				+ "_solve_P", computeP, "P");
+		FileDataSink outQ = new FileDataSink(new RecordOutputFormat(), output
+				+ "_solve_Q", computeQ, "Q");
+
+		ConfigBuilder configP = RecordOutputFormat.configureRecordFormat(outP)
+				.recordDelimiter('\n').fieldDelimiter(',')
+				.field(PactInteger.class, 0);
+		for (int i = 0; i < nFactors; i++) {
+			configP = configP.field(PactDouble.class, i + 1);
 		}
 
-		@Override
-		public void match(PactRecord ratings, PactRecord factors,
-				Collector<PactRecord> out) throws Exception {
-			PactRecord res = new PactRecord(nFactors + 3);
-			res.setField(0, ratings.getField(0, PactInteger.class));
-			res.setField(1, ratings.getField(1, PactInteger.class));
-			res.setField(2, ratings.getField(2, PactDouble.class));
-			for (int i = 0; i < nFactors; i++) {
-				res.setField(i + 3, factors.getField(i + 1, PactDouble.class));
-			}
-			out.collect(res);
+		ConfigBuilder configQ = RecordOutputFormat.configureRecordFormat(outQ)
+				.recordDelimiter('\n').fieldDelimiter(',')
+				.field(PactInteger.class, 0);
+		for (int i = 0; i < nFactors; i++) {
+			configQ = configQ.field(PactDouble.class, i + 1);
 		}
+
+		// Plan plan = new Plan(outQ, "ALS Example");
+		Plan plan = new Plan(new ArrayList<GenericDataSink>(
+				Arrays.asList(new GenericDataSink[] { outP, outQ })));
+		plan.setDefaultParallelism(numSubTasks);
+		return plan;
 	}
 
 	@Override
@@ -305,10 +283,6 @@ public class AlsMain implements PlanAssembler, PlanAssemblerDescription {
 	public static void main(String[] args) throws Exception {
 		AlsMain als = new AlsMain();
 		Plan plan = als.getPlan(args[0], args[1], args[2], args[3]);
-		// Plan plan = als.getPlan(args[0], args[1], args[2], args[3]);
-		// This will create an executor to run the plan on a cluster. We assume
-		// that the JobManager is running on the local machine on the default
-		// port. Change this according to your configuration.
 		PlanExecutor ex = new RemoteExecutor("localhost", 6123,
 				"target/pact-examples-0.4-SNAPSHOT-WordCount.jar");
 		ex.executePlan(plan);
